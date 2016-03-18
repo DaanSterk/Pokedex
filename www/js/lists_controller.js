615210ab274626024bf75ceaa530c491b7f03ba1
DaanSterk <daan-sterk@hotmail.com>
2016-03-18 12:44:39 +0100

    begin detail view

--- www/js/lists_controller.js
+++ www/js/lists_controller.js
@@ -10,7 +10,7 @@ function showAllPokemon(limit, offset) {
 	callbacks = limit;
 	callbacksLeft = limit;
 
-	showLoader();
+	showListLoader();
 	
 	$.ajax({
   		url: "http://pokeapi.co/api/v2/pokemon/?limit=" + limit + "&offset=" + offset,
@@ -21,13 +21,14 @@ function showAllPokemon(limit, offset) {
   		}
 	});
 }
+
 	// Subfunction - do not call directly. Adds a single pokemon to the 'ALL POKEMON' list.
 	function addSinglePokemonToAllList(data, num) {
 		var currPokemon = data.results[num];
 
 		var item = document.createElement("li");
 		var anchor = document.createElement("a");
-			 $(anchor).attr("href", "views/detail.html").attr("data-transition", "none");
+			 $(anchor).attr("data-transition", "none");
 		var image = document.createElement("img");
 		var h2 = document.createElement("h2");
 			$(h2).text(currPokemon.name);
@@ -36,6 +37,10 @@ function showAllPokemon(limit, offset) {
 		anchor.appendChild(image);
 		anchor.appendChild(h2);
 		anchor.appendChild(p);
+		$(anchor).click(function() {
+			showDetail(data.results[num].name);
+		});
+
 		item.appendChild(anchor);
 
 		$("#list_all_pokemon")[0].appendChild(item);
@@ -64,36 +69,67 @@ function showAllPokemon(limit, offset) {
 					hideLoader();
 				}
 				else {
-					showLoader();
+					showListLoader();
 				}
 	  		}
 		});
 	}
 
-	function isFullyLoaded() {
-		callbacksLeft--;
-		return callbacksLeft == 0;
-	}
+function isFullyLoaded() {
+	callbacksLeft--;
+	return callbacksLeft == 0;
+}
 
-	function showLoader() {
-		$("#list_all_pokemon").hide();
+function showListLoader() {
+	$("#list_all_pokemon").hide();
 
-	    $.mobile.loading( "show", {
-	            text: "Loading pokemon " + (callbacks - callbacksLeft) + "/" + callbacks,
-	            textVisible: true,
-	            theme: $.mobile.loader.prototype.options.theme,
-	            textonly: false
-	    });
-	}
+    $.mobile.loading( "show", {
+            text: "Loading pokemon " + (callbacks - callbacksLeft) + "/" + callbacks,
+            textVisible: true,
+            theme: $.mobile.loader.prototype.options.theme,
+            textonly: false
+    });
+}
 
-	function hideLoader() {
-		$.mobile.loading("hide");
-		$("#list_all_pokemon").show();
-		savedList = $("#list_all_pokemon");
-	}
+function showLoader() {
+	$.mobile.loading( "show", {
+		text: "Loading pokemon",
+		textVisible: true,
+		theme: $.mobile.loader.prototype.options.theme,
+		textonly: false
+	});
+}
+
+function hideLoader() {
+	$.mobile.loading("hide");
+	$("#list_all_pokemon").show();
+	savedList = $("#list_all_pokemon");
+}
+
+function showDetail(name) {
+	$.mobile.changePage("../views/detail.html");
+	$.ajax({
+		url: "http://pokeapi.co/api/v2/pokemon/" + name,
+		success: function(data) {
+			data = JSON.parse(data); // Somehow returned a string instead of JSON object.
+			$(".main-title").text(data.name);
+			$("#poke_img1").attr("src", data.sprites.front_default);
+			$("#poke_img2").attr("src", data.sprites.back_default);
+			$("#poke_img3").attr("src", data.sprites.front_shiny);
+			$("#poke_img4").attr("src", data.sprites.back_shiny);
+			hideLoader();
+		}
+	});
+}
 
 $(document).ready(function(){
-	showAllPokemon(10, 150);
+	showAllPokemon(1, 150);
 });
 
+$(document).ajaxStart(function() {
 
+});
+
+$(document).ajaxStop(function() {
+
+});
