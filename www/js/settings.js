e62772378dbfe9e7475a282edd5fb08fe20fdfe9
Jeffrey Vervoort <jeffreyvervoort@gmail.com>
2016-03-21 14:14:55 +0100

    Listeners werken nu de rest nog van de settings pagina

--- www/js/settings.js
+++ www/js/settings.js
@@ -5,34 +5,38 @@ var settings = {
     },
 
     setSettingValues: function(){
-        if(localStorage.getItem("awesomesettings") == 'on'){
-            $('#switch-awesome-settings').val('on').slider('refresh');
-        };
-        if(localStorage.getItem("showcompass") == 'on'){
-            $('#switch-show-compass').val('on').slider('refresh');
-        };
-        if(localStorage.getItem("showgps") == 'on'){
-            $('#switch-show-gps').val('on').slider('refresh');
-        };
+        $(document).on("pageshow", '#settings-page', function () {
+            if (localStorage.getItem("awesomesettings") == true) {
+                $('#switch-awesome-settings').prop('checked', true).slider('refresh');
+                console.log("Settings is: " + $('#switch-awesome-settings').is(":checked"));
+            }
+            ;
+            if (localStorage.getItem("showcompass") == true) {
+                $('#switch-show-compass').prop('checked', true).slider('refresh');
+                console.log("Compass is: " + $('#switch-show-compass').is(":checked"));
+            }
+            ;
+            if (localStorage.getItem("showgps") == true) {
+                $('#switch-show-gps').prop('checked', true).slider('refresh');
+                console.log("GPS is: " + $('#switch-show-gps').is(":checked"));
+            }
+            ;
+        });
     },
 
     addListeners: function(){
-        console.log($('#switch-awesome-settings'));
-        console.log($('#switch-show-compass'));
-        console.log($('#switch-show-gps'));
-
-        $('#settings-page').on("pageshow", function () {
-            $('#switch-awesome-settings').on("tap", function(){
-                console.log("switch settings");
-                localStorage.setItem("awesomesettings", $('#switch-awesome-settings').val());
+        $('body').on("pageshow", '#settings-page', function () {
+            $('#switch-awesome-settings').change(function(){
+                console.log($('#switch-awesome-settings').is(":checked"));
+                localStorage.setItem("awesomesettings", $('#switch-awesome-settings').is(":checked"));
             });
             $('#switch-show-compass').change(function(){
-                console.log("switch compass");
-                localStorage.setItem("showcompass", $('#switch-show-compass').val());
+                console.log($('#switch-show-compass').is(":checked"));
+                localStorage.setItem("showcompass", $('#switch-show-compass').is(":checked"));
             });
-            $('#switch-show-gps').on("change", function(){
-                console.log("switch gps");
-                localStorage.setItem("showgps", $('#switch-show-gps').val());
+            $('#switch-show-gps').change(function(){
+                console.log($('#switch-show-gps').is(":checked"))
+                localStorage.setItem("showgps", $('#switch-show-gps').is(":checked"));
             });
         });
     }
