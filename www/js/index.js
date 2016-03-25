c9bf77f9c339a43796389717fdf278f004d8a3a4
Jeffrey Vervoort <jeffreyvervoort@gmail.com>
2016-03-25 14:16:45 +0100

    Openen van de facebook app.

--- www/js/index.js
+++ www/js/index.js
@@ -49,5 +49,10 @@ var app = {
         $(document).on("tap", "#pokedex-webpage", function () {
             window.open("http://www.pokemon.com/nl/pokedex/", "_system");
         });
+
+        $(document).on("tap", "#pokedex-facebook", function () {
+            //window.open("https://www.facebook.com/1399971336935582", "_system");
+            window.open('fb://page/1399971336935582', '_system');
+        });
     }
 };
